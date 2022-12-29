2868f36e099780c5c2d76f558edecdfd8684fdb2
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2022-12-29 14:55:24 +0300

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,84 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `Please provide personalized advice or guidance on 
+how to improve mental health or well-being, 
+based on your professional experience as a therapist or 
+psychologist: `;
+
+const generateAction = async (req, res) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
+
+
+
+// Incase if I ever want to improve my prompts through 
+// the process of prompt chaining
+// const basePromptPrefix =
+// `
+// Write me a detailed table of contents for a blog post with the title below.
+
+// Title:
+// `
+
+// const generateAction = async (req, res) => {
+//   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${basePromptPrefix}${req.body.userInput}`,
+//     temperature: 0.8,
+//     max_tokens: 250,
+//   });
+
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+
+//   // I build Prompt #2.
+//   const secondPrompt = 
+//   `
+//   Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+//   Title: ${req.body.userInput}
+
+//   Table of Contents: ${basePromptOutput.text}
+
+//   Blog Post:
+//   `
+
+//   // I call the OpenAI API a second time with Prompt #2
+//   const secondPromptCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${secondPrompt}`,
+//     // I set a higher temperature for this one. Up to you!
+//     temperature: 0.85,
+// 		// I also increase max_tokens.
+//     max_tokens: 1250,
+//   });
+
+//   // Get the output
+//   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+//   // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+//   res.status(200).json({ output: secondPromptOutput });
+// };
+
+// export default generateAction;
---
+++ pages/index.js
@@ -0,0 +1,79 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput,setApiOutput]= useState('')
+  const [isGenerating,setIsGenerating]=useState(false)
+
+  const callGenerateEndpoint = async() => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify({userInput}),
+    });
+
+    const data = await response.json();
+    const {output} = data;
+    console.log("OpenAI replied...",output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  }
+
+  return (
+    <div className="root">
+      <Head>
+        <title>Therapy Sense</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Therapy Sense</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Therapy Sense is a virtual therapy AI that provides support and resources for mental health and emotional well-being.</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+        <div className="prompt-buttons">
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+            <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p> }
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+        )}
+      </div>
+      <div className='footer'>
+          <p>Developed by <span class="colorfill"><a href="https://www.linkedin.com/in/chris-vaz/">Chris Vaz</a></span>© 2022-2023</p>
+      </div>
+    </div></div>
+  );
+};
+
+export default Home;
